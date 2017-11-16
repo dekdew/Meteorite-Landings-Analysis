8dc362ab3c46813f119be9997626b260bb9c01ca
Passawit Riewthong <passawit1999@gmail.com>
2017-11-16 13:51:40 +0700

    update search bar

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def index():
+    return render_template('index.html')
+
+@app.route('/search', methods = ['GET'])
+def search():
     search = request.args.get('search', None)
-    return render_template('index.html', search=search)
+    return render_template('search.html', search=search)
 
 if __name__ == "__main__":
     app.run(debug=True)
