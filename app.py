a92fc844c0f1e41cd4e17281b26a904240d7daf5
Passawit Riewthong <passawit1999@gmail.com>
2017-11-16 15:30:56 +0700

    update search data

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from data import call_meteorite
 
 app = Flask(__name__)
 
@@ -9,7 +10,8 @@ def index():
 @app.route('/search', methods = ['GET'])
 def search():
     search = request.args.get('search', None)
-    return render_template('search.html', search=search)
+    m_class, mass, fall, year, geolocation = call_meteorite.call(search)
+    return render_template('search.html', search=search, m_class=m_class, mass=mass, fall=fall, year=year, location=geolocation)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ data/call_meteorite.py
@@ -0,0 +1,40 @@
+import json
+
+def call_data():
+    """call data from data.json and return data"""
+    with open('data.json') as data_file:
+        data = json.load(data_file)
+    return data
+
+def call(met):
+    data = call_data()
+    m_class, mass, fall, year, geolocation = 'n/a', 'n/a', 'n/a', 'n/a', 'n/a'
+
+    for i in data:
+        if met.lower() == i['name'].lower():
+            try:
+                m_class = i['recclass']
+            except KeyError: # check if no data
+                m_class = 'n/a'
+
+            try:
+                mass = i['mass']
+            except KeyError: # check if no data
+                mass = 'n/a'
+
+            try:
+                fall = i['fall']
+            except KeyError: # check if no data
+                fall = 'n/a'
+
+            try:
+                year = i['year'][:4].strip('-')
+            except KeyError: # check if no data
+                year = 'n/a'
+
+            try:
+                geolocation = i['geolocation']['coordinates']
+            except KeyError: # check if no data
+                location = 'n/a'
+
+    return m_class, mass, fall, year, geolocation
